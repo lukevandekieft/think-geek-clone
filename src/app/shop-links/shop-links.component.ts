5d764e4d86b1008e6b9d835465e9e4ce885aa134
Luke Vandekieft <vandekie@gmail.com>
2018-10-19 14:36:18 -0700

    Create link list in shoplinks section.

--- src/app/shop-links/shop-links.component.ts
+++ src/app/shop-links/shop-links.component.ts
@@ -10,7 +10,15 @@ import { Link } from '../models/link-model';
 
 export class ShopLinksComponent implements OnInit {
   navLinks: Link[] = [
-    new Link('What\'s New', 'https://www.thinkgeek.com/whats-new/')
+    new Link('What\'s New', 'https://www.thinkgeek.com/whats-new/'),
+    new Link('Bestsellers', 'https://www.thinkgeek.com/bestsellers/'),
+    new Link('Exclusives', 'https://www.thinkgeek.com/interests/exclusives/'),
+    new Link('Gifts', 'https://www.thinkgeek.com/gifts/'),
+    new Link('Timmy\'s Last Call', 'https://www.thinkgeek.com/interests/timmys-last-call/'),
+    new Link('T-Shirts', 'https://www.thinkgeek.com/clothing/t-shirts/'),
+    new Link('Collectibles', 'https://www.thinkgeek.com/collectibles/'),
+    new Link('Geekpoints', 'https://www.thinkgeek.com/geekpoints/'),
+    new Link('On Sale', 'https://www.thinkgeek.com/clearance/on-sale/')
   ]
   constructor() { }
 
